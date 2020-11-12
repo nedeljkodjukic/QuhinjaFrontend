5ad48f4694a2f7799c6f6f27ac1a007cb74112b3
5ardjordjevic <petardjordjevi.1@gmail.com>
2020-11-12 16:33:30 +0100

    Finished HomePage

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,23 +3,19 @@
     <q-header elevated>
       <q-toolbar
         style=" height:20px;display:flex; flex-direction:row; flex grow:1; align-content:center  "
-        class="bg-amber-6"  
+        class="bg-amber-6"
       >
-        <h6 class="text-brown-5 q-mr-sm">Quhinja</h6>
+        <h6 class="text-brown-9 q-mr-sm">Quhinja</h6>
         <q-btn :to="`/`" dense>
-          <q-icon name="home" color="brown-5"> </q-icon>
+          <q-icon name="home" color="brown-9"> </q-icon>
         </q-btn>
-        <q-tabs class="text-brown-5">
+        <q-tabs class="text-brown-9">
           <q-route-tab active name="Menu" label="Menu" to="/menu" />
           <q-route-tab name="Jela" label="Jela" to="/dishes" />
-          <q-route-tab
-            name="Zaposleni"
-            label="Zaposleni"
-            to="/employees"
-          />
+          <q-route-tab name="Zaposleni" label="Zaposleni" to="/employees" />
         </q-tabs>
-          <q-btn class="fixed-top-right on-left q-mt-sm" dense>
-          <q-icon name="login" color="brown-5"> </q-icon>
+        <q-btn class="fixed-top-right on-left q-mt-sm" dense>
+          <q-icon name="login" color="brown-9"> </q-icon>
         </q-btn>
       </q-toolbar>
     </q-header>
@@ -36,9 +32,7 @@ export default {
   data() {
     return {};
   },
-  methods: {
-  
-  },
+  methods: {},
 };
 </script>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,40 +1,124 @@
 <template>
-  <q-page class="pozadina flex flex-center">
-
-    <div v-for="t in dishes" :key="t.id">
-      {{`${t.name}:`}}
+  <q-page class="column items-center q-gutter-y-none bg-grey-4">
+    <q-parallax
+      :speed="1"
+      src="../../public/test.jpg"
+      style="border-color: #233D53; border-style: solid; border-width: 5px 0px"
+    >
+    </q-parallax>
+    <div class="text-h5 text-brown-9 q-py-md">
+      Najbolje ocenjena jela
+      <q-separator color="brown-9" size="4px" />
     </div>
+
+    <q-carousel
+      v-model="selectedDishIndex"
+      swipeable
+      infinite
+      animated
+      padding
+      arrows
+      class="bg-transparent"
+      transition-prev="fade"
+      transition-next="fade"
+      control-color="brown-9"
+    >
+      <q-carousel-slide
+        class="q-pa-sm"
+        v-for="(dish, index) in dishes"
+        :key="index"
+        :name="index"
+      >
+        <div class="row q-gutter-x-md justify-center items-center">
+          <div
+            class="column items-center"
+            v-for="adjacentIndex in adjacentIndexes(index)"
+            :key="adjacentIndex"
+          >
+            <q-card class="my-card bg-amber-6">
+              <q-img src="../../public/punjenapaprika.jpg" />
+
+              <q-card-section>
+                <div class="row no-wrap items-center">
+                  <div class="col text-h6 ellipsis">
+                    {{ dishes[adjacentIndex].name }}
+                  </div>
+                </div>
+
+                <q-rating
+                  class="text-brown-7"
+                  v-model="stars"
+                  :max="5"
+                  size="32px"
+                />
+              </q-card-section>
+
+              <q-card-section class="q-pt-none">
+                <div class="text-subtitle1 text-blue-9">
+                  {{ dishes[adjacentIndex].dishType }}
+                </div>
+                <div class="text-caption text-grey-9">
+                  {{ dishes[adjacentIndex].description }}
+                </div>
+              </q-card-section>
+
+              <q-separator />
+
+              <q-card-actions>
+                <q-icon flat round name="double_arrow" />
+                <q-btn flat color="blue-9"> Detalji </q-btn>
+              </q-card-actions>
+            </q-card>
+          </div>
+        </div>
+      </q-carousel-slide>
+    </q-carousel>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "PageIndex",
-  data(){
+  data() {
     return {
-      dishes:[]
-    }
+      stars: 3,
+      dishes: [],
+      selectedDishIndex: 0,
+    };
   },
-  computed:{
-  
-  },
+  computed: {},
 
-methods:{
-getData(){
- this.$store.dispatch('apiRequest/getApiRequest', { url: 'Dish' })
-      .then(res => (this.dishes = res))
-  
-}
-},
- created () {
-   this.getData();
-  }
-  }
+  methods: {
+    getData() {
+      this.$store
+        .dispatch("apiRequest/getApiRequest", { url: "Dish" })
+        .then((res) => (this.dishes = res));
+    },
+    adjacentIndexes(index) {
+      const length = 6;
+      const array = [];
+      if (this.numOfShowedItems === 5) {
+        array.push((index - 2 + length) % length);
+      }
+      array.push((index - 1 + length) % length);
+      array.push(index);
+      array.push((index + 1) % length);
+      if (this.numOfShowedItems === 5) {
+        array.push((index + 2) % length);
+      }
+      return array.reverse();
+    },
+  },
+  created() {
+    this.getData();
+  },
+  computed: {
+    numOfShowedItems() {
+      if (this.$q.screen.gt.sm) return 5;
+      else return 3;
+    },
+  },
+};
 </script>
 <style scoped>
-.pozadina{
-  background-image: url('../../public/test.jpg');
-  background-position: center; /* Center the image */
-background-size: cover;
-}
 </style>
