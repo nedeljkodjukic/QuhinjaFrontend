adb0b7c082bfab0962398f12329ded30c0ab85ff
5ardjordjevic <petardjordjevi.1@gmail.com>
2020-12-08 10:58:05 +0100

    f

--- quasar.conf.js
+++ quasar.conf.js
@@ -114,8 +114,8 @@ module.exports = function (/* ctx */) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Quhinja`,
+        short_name: `Quhinja`,
         description: `A Quasar Framework app`,
         display: 'standalone',
         orientation: 'portrait',
